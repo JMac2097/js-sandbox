4118e72707ed074f006e69d9f73a1886bb5309f1
John McKie <john.mckie@remarkable.net>
2018-04-13 15:42:07 +0100

    Task filter in place

--- task-list/app.js
+++ task-list/app.js
@@ -2,7 +2,7 @@
 
 const form = document.querySelector("#task-form");
 const taskList = document.querySelector(".collection");
-const cleatBtn = document.querySelector(".clear-tasks");
+const clearBtn = document.querySelector(".clear-tasks");
 const filter = document.querySelector("#filter");
 const taskInput = document.querySelector("#task");
 
@@ -12,6 +12,12 @@ loadEventListeners();
 function loadEventListeners() {
     //add Task event
     form.addEventListener("submit", addTask);
+    //remove task event
+    taskList.addEventListener("click", removeTask);
+    //clear task event
+    clearBtn.addEventListener("click", clearTasks);
+    // Filter tasks event
+    filter.addEventListener("keyup", filterTasks);
 }
 
 // Add Task
@@ -41,4 +47,38 @@ function addTask(e) {
     taskInput.value = '';
 
     e.preventDefault();
+}
+
+//remove task
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+//clear all tasks
+function clearTasks() {
+    if (confirm('Are you sure you wish to clear all tasks?')) {
+        while(taskList.firstChild) {
+            taskList.removeChild(taskList.firstChild);
+        }
+    }
+}
+
+// Filter Tasks
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(
+        function(task) {
+        const item = task.firstChild.textContent;
+            if(item.toLowerCase().indexOf(text) != -1) {
+                task.style.display = 'block';
+            } else {
+                task.style.display = 'none';
+            };
+    });
+
 }
