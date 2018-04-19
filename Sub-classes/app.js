a5b491b445504c10fd2fa61fef4f59bd7f64eb9b
John McKie <john.mckie@remarkable.net>
2018-04-19 14:14:45 +0100

    Some more work on class extensions

--- Sub-classes/app.js
+++ Sub-classes/app.js
@@ -30,3 +30,51 @@ console.log(john.greeting());
 
 console.log(Customer.getMembershipCost());
 
+
+
+
+
+
+
+// Create Human class and assign params
+// Create SuperHuman and Extend Human
+// Setup Two normals and two heroes
+
+
+class Human {
+    constructor(firstName, lastName) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+    };
+
+    greet() {
+        return `Hello ${this.firstName} ${this.lastName}`;
+    };
+};
+
+const derek = new Human('Derek', 'Smalls');
+
+console.log(derek);
+console.log(derek.greet());
+
+class SuperHuman extends Human {
+    constructor(firstName, lastName, power1, power2) {
+        super(firstName, lastName);
+
+        this.power1 = power1;
+        this.power2 = power2;
+
+    };
+
+    powers(weakness) {
+        return `${this.firstName} ${this.lastName} has the power of ${this.power1} and ${this.power2}, but suffers from a fear of ${weakness}`
+    };
+
+};
+
+const newSuperHero = new SuperHuman('Mr', 'Awesome', 'Super Speed', 'Flight');
+
+console.log(newSuperHero);
+console.log(newSuperHero.powers('arse magic'));
