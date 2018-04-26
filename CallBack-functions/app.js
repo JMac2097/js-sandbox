61afd1ea9f61308271c0a40f21452476f0596d55
John McKie <john.mckie@remarkable.net>
2018-04-26 09:19:58 +0100

    Tiny text change

---
+++ CallBack-functions/app.js
@@ -0,0 +1,45 @@
+const posts = [
+    {title: "Post 1", body: "Thhis is post One"},
+    {title: "Post 2", body: "Thhis is post Two"}
+    
+];
+
+
+// function createPost(post) {
+//     setTimeout(function() {
+//         posts.push(post);
+//     }, 2000);
+// };
+
+// function getPosts() {
+//     setTimeout(function() {
+//         let output = '';
+//         posts.forEach(function(post) {
+//             output += `<li>${post.title}</li>`
+//         });
+//         document.body.innerHTML = output;
+//     }, 1000);
+// };
+
+// createPost({title: "Post 3", body: "Thhis is post Three"});
+
+// getPosts();
+
+function createPost(post, callback) {
+    setTimeout(function() {
+        posts.push(post);
+        callback();
+    }, 2000);
+};
+
+function getPosts() {
+    setTimeout(function() {
+        let output = '';
+        posts.forEach(function(post) {
+            output += `<li>${post.title}</li>`
+        });
+        document.body.innerHTML = output;
+    }, 1000);
+};
+
+createPost({title: "Post 3", body: "This is post Three"}, getPosts);
