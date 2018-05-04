89803c980aea2db84e2cbd77d309f1fe211e0d3f
John McKie <john.mckie@remarkable.net>
2018-05-04 15:21:47 +0100

    Weather app

---
+++ WeatherJS/app.js
@@ -0,0 +1,37 @@
+// init storage
+const storage = new Storage();
+// Get stored location data
+const weatherLocation = storage.getLocationData();
+// init weather object
+const weather = new Weather( weatherLocation.city, weatherLocation.state );
+// init UI object
+const ui = new UI();
+
+// Get weather on DOM load
+document.addEventListener('DOMContentLoaded', getWeather);
+
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+    
+    // Change location
+    weather.changeLocation(city, state);
+
+    // Set Location in LS
+    storage.setLocationData(city, state);
+
+    // Get weather again
+    getWeather();
+
+    // Close modal
+    $('#locModal').modal('hide');
+});
+
+function getWeather() {
+    weather.getWeather()
+    .then(results => {
+        ui.paint(results);
+    })
+    .catch(err => console.log(err));
+};
