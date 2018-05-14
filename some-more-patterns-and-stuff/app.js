21e63b71a94ebac636ae12a55f4e8ab986f5e688
John McKie <john.mckie@remarkable.net>
2018-05-14 16:55:59 +0100

    Start of the Observer pattern

--- some-more-patterns-and-stuff/app.js
+++ some-more-patterns-and-stuff/app.js
@@ -48,8 +48,8 @@
 //     };
 //
 // })();
-
-
+//
+//
 // Exposer.first();
 // Exposer.second();
 // Exposer.methodToExpose;  //Undefined
@@ -109,4 +109,70 @@
 //
 // }();
 
+// //  Factory Class
 
+// function MemberFactory() {
+//     this.createMember = function(name, type) {
+//         let member; 
+
+//         if(type === 'simple') {
+//             member = new SimpleMembership(name);
+//         } else if(type === 'standard') {
+//             member = new StandardMembership(name);
+//         } else if(type === 'super') {
+//             member = new SuperMembership(name);
+//         }
+
+//         member.type = type;
+
+//         member.define = function() {
+//             console.log(`${this.name} (${this.type}): ${this.cost}`);
+//         }
+
+//         return member;
+//     }
+// };
+
+// const SimpleMembership = function(name) {
+//     this.name = name;
+//     this.cost = '$5';
+// };
+
+// const StandardMembership = function(name) {
+//     this.name = name;
+//     this.cost = '$15';
+// };
+
+// const SuperMembership = function(name) {
+//     this.name = name;
+//     this.cost = '$25';
+// };
+
+
+// const members = [];
+// const factory = new MemberFactory();
+
+// members.push(factory.createMember('John Doe', 'simple'));
+// members.push(factory.createMember('Doe Doe', 'standard'));
+// members.push(factory.createMember('Jane Doe', 'super'));
+
+
+
+// // console.log(members);
+
+// members.forEach(function(member) {
+//     member.define();
+// });
+
+
+//  Observer Pattern
+function EventObserver() {
+    this.observers = [];
+};
+
+EventObserver.prototype = {
+    subscribe: function(fn) {
+        this.observers.push(fn);
+        console.log(`You are now subscribed to ${fn.name}`)
+    }
+};
