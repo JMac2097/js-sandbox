643f524078a85a94c4641681474ed728a22c2132
John McKie <therealjmac2097@gmail.com>
2018-06-03 13:01:33 +0100

    Logic working

--- js-prime-number-finder/app.js
+++ js-prime-number-finder/app.js
@@ -11,26 +11,31 @@ document.getElementById('submit').addEventListener('click', findThePrimes);
 
 function findThePrimes() {
 
-    let startNum = document.getElementById('startNumber').value;  
-    let endNum = document.getElementById('endNumber').value;
+    // Set variables for counting
+    let startNum = parseInt(document.getElementById('startNumber').value), 
+        endNum = parseInt(document.getElementById('endNumber').value);
+
     for (let counter = startNum; counter <= endNum; counter++) {
         
-        let notPrime = false;
-        for (let i = 2; i <= counter; i++) {
-            if (counter % i === 0 && i!==counter) {   // if counter modulus i equals zero AND i doesn't not equal counter 
+        if ( counter !== 0 && counter !== 1 )  {
             
-            notPrime = true;
-
+            let notPrime = false;
+            for (let i = 2; i <= counter; i++) {
+                if (counter % i === 0 && i!==counter) {   // if counter modulus i equals zero AND i doesn't not equal counter 
+                
+                notPrime = true;
+                
+                };
             };
-        };
-
-        if (notPrime === false) {
-            numArr.push(counter);
-        };
+        
+            if (notPrime === false) {
+                numArr.push(counter);
+            };
+        }
     
     };
 
-// console.log(numArr);
+console.log(numArr);
 
 console.log(`Number of prime numbers between ${startNum} and ${endNum} are: ${numArr.length}`);
 
